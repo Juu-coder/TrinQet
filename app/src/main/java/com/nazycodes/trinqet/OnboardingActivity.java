0ee2f1a9a282bb640b04129e11feb81c635f5164
Chinaza <ogwo.chinaza@yahoo.com>
2020-10-03 09:35:36 +0100

    design onboarding screen

--- app/src/main/java/com/nazycodes/trinqet/OnboardingActivity.java
+++ app/src/main/java/com/nazycodes/trinqet/OnboardingActivity.java
@@ -1,14 +1,30 @@
 package com.nazycodes.trinqet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.nazycodes.trinqet.adapters.CustomOnboardingPageAdapter;
+
+import me.relex.circleindicator.CircleIndicator;
+
 public class OnboardingActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private CircleIndicator indicator;
+    CustomOnboardingPageAdapter onboardingPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding);
+
+        viewPager = findViewById(R.id.viewPager);
+        onboardingPagerAdapter = new CustomOnboardingPageAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(onboardingPagerAdapter);
+
+        indicator = findViewById(R.id.indicator);
+        indicator.setViewPager(viewPager);
     }
 }
---
+++ app/src/main/java/com/nazycodes/trinqet/fragments/OnboardingFragment2.java
@@ -0,0 +1,23 @@
+package com.nazycodes.trinqet.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.nazycodes.trinqet.R;
+
+public class OnboardingFragment2 extends Fragment {
+    public OnboardingFragment2() {}
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.onbording_page2, container, false);
+
+        return view;
+    }
+}
