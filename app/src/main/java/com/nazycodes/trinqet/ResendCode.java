47eaa6d0b7520def6065face4eca20a150bf6926
Juu-coder <onyin4great@gmail.com>
2020-10-03 14:14:17 -0700

    'new'

---
+++ app/src/main/java/com/nazycodes/trinqet/ResendCode.java
@@ -0,0 +1,14 @@
+package com.nazycodes.trinqet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResendCode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resend_code);
+    }
+}
